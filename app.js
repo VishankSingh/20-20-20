ad8d39c46e9076888493e549971e1e5e3608676e
VishankSingh <68466318+VishankSingh@users.noreply.github.com>
2020-12-08 16:02:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,28 @@
-let date; //declareing date variable
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var interval = localStorage.getItem("mins");
 
 function notificationSendMain() {
-    if(!window.Notification) {
-        console.log('Browser does not support notifications.');
-
-    } else {
-
-        if (Notification.permission === 'granted') {
-            var notify = new Notification('', {
-                body: "You'll get a notification every 20 minutes which will prompt you to look away from the screen.",
-                icon: "favicon.png"
-            });
-            notificationSend()
-        } else {
-            Notification.requestPermission().then(function(p) {
-                if(p === 'granted') {
-                    var notify = new Notification('', {
-                        body: "You'll get a notification every 20 minutes which will prompt you to look away from the screen.",
-                        icon: "favicon.png"
-                    });
-                    notificationSend()
-                } else {
-                    console.log('User blocked notifications.');
-                }
-            }).catch(function(err) {
-                console.error(err);
-            });
-        }
-    }
-}
-
-function notificationSend() {
-    date = new Date();
-    seconds = date.getSeconds();
-    minutes = date.getMinutes();
-    milliSeconds = date.getMilliseconds();
-    timeConstant = (minutes*60*1000)+(seconds*1000)+milliSeconds
-    
-    
-    setTimeout(() => { 
+    if (Notification.permission === 'granted') {
         var notify = new Notification('', {
-            body: "Please look 20 feet away for 20 seconds",
+            body: "You'll get a notification every " + interval + " minutes which will prompt you to look away from the screen.",
             icon: "favicon.png"
         });
-    }, 1);
- 
-    setTimeout(() => {notificationSend()}, 1);
+    } 
+        
+    else {
+        Notification.requestPermission().then(function(p) {
+            if(p === 'granted') {
+                var notify = new Notification('', {
+                    body: "You'll get a notification every " + interval + " minutes which will prompt you to look away from the screen.",
+                    icon: "favicon.png"
+                });
+            } 
+                
+            else {
+                console.log('User blocked notifications.');
+            }
 
+        }).catch(function(err) {
+            console.error(err);
+        });
+    }
 }
